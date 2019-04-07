55c1f9b376aeebd145a0dd61191f97c706e7128c
Timmy <timmycryptocoin@gmail.com>
2019-04-08 01:08:36 +1000

    Heroku adjust

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use('/api/todos', todos)
 
 //For Heroku deployment
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
